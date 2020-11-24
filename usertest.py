ecddb4fa67f4f8e06470f23d638e0ea3777acd8c
Elizabeth Nderitu <nderituliz@gmail.com>
2020-11-24 17:29:13 +0300

    Add setup and teardown methods

--- usertest.py
+++ usertest.py
@@ -10,40 +10,4 @@ class UserTest(unittest.TestCase):
 
         User.user_list = []
 
-    def test__init(self):
-        self.assertEqual(self.new_user.username , "nderituliz")
-        self.assertEqual(self.new_user.email,"nderituliz@gmail.com")
-        self.assertEqual(self.new_user.password,"password123")
-
-    def test_save_user(self):
-
-        self.new_user.save_user()
-        self.assertEqual(len(User.user_list),1)
-
-
-    def test_save_multiple_users(self):
-
-        self.new_user.save_user()
-        test_user = User("test", "nderituliz@gmail.com", "password123")
-        test_user.save_user()
-        self.assertEqual(len(User.user_list),2)
-
-    def test_delete_user(self):
-
-        self.new_user.save_user()
-        test_user = User("test", "nderituliz@gmail.com", "password123")
-        test_user.save_user()
-        self.new_user.delete_user()
-        self.assertEqual(len(User.user_list),1)
-
-    def test_search_user(self):
-
-        self.new_user.save_user()
-        test_user = User("test", "nderituliz@gmail.com", "password123")
-        test_user.save_user()
-        user_found = User.search_user("nderituliz")
-        self.assertEqual(user_found.username, self.new_user.username)
-
-
-if __name__ = '__main__':
-    unittest.main()
+    
