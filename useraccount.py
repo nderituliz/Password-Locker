e32582747c6d1d3524656bfec64039bb4a19dc12
Elizabeth Nderitu <nderituliz@gmail.com>
2020-11-24 17:18:10 +0300

    Add init method to create our account objects

--- useraccount.py
+++ useraccount.py
@@ -10,44 +10,4 @@ class UserAccount:
         self.email= email
         self.password = password
 
-    def save_user_details(self):
-
-        UserAccount.user_pass_list.append(self)
-
-    def delete_user_details(self):
-
-        UserAccount.user_pass_list.remove(self)
-
-    @classmethod
-    def search_user_account(cls,account):
-
-        for user_deets in cls.user_pass_list:
-            if user_deets.account == account:
-                return user_deets
-
-
-    @classmethod
-    def check_user_account(cls,account):
-        """ check if a user account exists """
-
-        for user_account_deets in cls.user_pass_list:
-            if user_account_deets.account == account:
-                return True
-            return False
-
-    
-    @classmethod
-    def show_user_details(cls):
-
-        return cls.user_pass_list
-
-    @classmethod
-    def copy_pwd(cls,password):
-        search_acc = UserAccount.search_user_account(password)
-        pyperclip.copy(search_acc.password)
-    
-
-
-    
-
-    
+   
