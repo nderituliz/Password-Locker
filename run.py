93cbe630b60a385ee883f7b06c011a1e812e9050
Elizabeth Nderitu <nderituliz@gmail.com>
2020-11-24 17:35:47 +0300

    Add method to search user , create user details and save user credentials

--- run.py
+++ run.py
@@ -15,3 +15,14 @@ def save_user(user):
 def save_user_account_details(user_details):
     user_details.save_user_account_details
 
+def search_user(username):
+    return User.search_user(username)
+
+def create_user_details(account,email,password):
+
+    new_account = UserAccount(account,email,password)
+    return new_account
+
+def save_user_credentials(user_cred):
+    user_cred.save_user_details()
+
