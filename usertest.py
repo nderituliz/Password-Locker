1c84b5d847a89fa9528613d0048a544a6aac3630
Elizabeth Nderitu <nderituliz@gmail.com>
2020-11-24 17:30:03 +0300

    Add test init and test save user methods

--- usertest.py
+++ usertest.py
@@ -10,4 +10,15 @@ class UserTest(unittest.TestCase):
 
         User.user_list = []
 
+    def test__init(self):
+        self.assertEqual(self.new_user.username , "nderituliz")
+        self.assertEqual(self.new_user.email,"nderituliz@gmail.com")
+        self.assertEqual(self.new_user.password,"password123")
+
+    def test_save_user(self):
+
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
+
+
     
