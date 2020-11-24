4f55e0b769f56cc73e6b4e355bb871c23e91d124
Elizabeth Nderitu <nderituliz@gmail.com>
2020-11-24 17:25:26 +0300

    Add test init methods and methods to test the save user and save multple accounts methods

--- useracounttest.py
+++ useracounttest.py
@@ -10,4 +10,22 @@ class UserAccountTest(unittest.TestCase):
     def tearDown(self):
         UserAccount.user_pass_list = []
 
+    def test_init(self):
+
+        self.assertEqual(self.new_account.account,"Gmail")
+        self.assertEqual(self.new_account.email,"ndertuliz@gmail.com")
+        self.assertEqual(sel.new_account.password,"password123")
+
+    def test_save_user(self):
+
+        self.new_account.save_user_details()
+        self.assertEqual(len(UserAccount.user_pass_list),1)
+
+    def test_saving_multiple_accounts(self):
+
+        self.new_account.save_user_details()
+        test_newaccount = UserAccount("Gmail","User","Password")
+        test_newaccount.save_user_details()
+        self.assertEqual(len(UserAccount.user_pass_list),2)
+
     
